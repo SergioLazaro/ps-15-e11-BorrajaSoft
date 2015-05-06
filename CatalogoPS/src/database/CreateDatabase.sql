d2d09ae08b58f908a2861df40a371aef80f37bcc
Lucas Doe Santos <lucasDoe@hotmail.es>
2015-05-06 16:24:14 +0200

    Base de datos 1.1
    Actualizada la base de datos.

--- CatalogoPS/src/database/CreateDatabase.sql
+++ CatalogoPS/src/database/CreateDatabase.sql
@@ -1,57 +1,78 @@
-DROP TABLE IF EXISTS ShoppingCarts;
-CREATE TABLE ShoppingCart (
-   workerID          INT(11)           NOT NULL,
-   productID         VARCHAR(45)       NOT NULL,
-   stock             INT(4)            NOT NULL
-   //orderID         DATE              NOT NULL ???
-);
+DROP TABLE IF EXISTS ProductTypes;
+CREATE TABLE ProductTypes (
+   productTypeID        INT(9)         NOT NULL AUTO_INCREMENT,
+   clothes              VARCHAR(45)    NOT NULL,
+   colour               VARCHAR(15)    NOT NULL,
+   style                VARCHAR        NOT NULL,
+   image                VARCHAR        NOT NULL,
+   size                 VARCHAR        NOT NULL,
 
-
-DROP TABLE IF EXISTS SaleRecords;
-CREATE TABLE RecordSale (
-   workerID          INT(11)           NOT NULL,
-   productID         INT(11)           NOT NULL,
-   date              DATE              NOT NULL,
-   numItems          DECIMAL(20,2)     NOT NULL,
-   orderID           INT(11)           NOT NULL
+   PRIMARY KEY (productTypeID)
 );
 
 
 DROP TABLE IF EXISTS Products;
-CREATE TABLE Products` (
-   productID         INT(11)           NOT NULL,
-   productTypeID     INT(11)           NOT NULL,
-   price             DECIMAL(X)        NOT NULL,
-   name              VARCHAR(45)       NOT NULL,
-   brand             VARCHAR(45)       NOT NULL,
-   stock             INT(11)           NOT NULL,
+CREATE TABLE Products (
+   productID            INT(9)         NOT NULL,
+   productTypeID        INT(9)         NOT NULL,
+   brand                VARCHAR(25)    NOT NULL,
+   name                 VARCHAR(45)    NOT NULL,
+   price                DECIMAL(5,2)   NOT NULL,
+   stock                INT(11)        NOT NULL,
 
    PRIMARY KEY (productID)
 );
 
 
-DROP TABLE IF EXISTS Workers;
-CREATE TABLE Workers (
-   workerID          INT(11)           NOT NULL,
-   name              VARCHAR(45)       NOT NULL,
-   mail              VARCHAR(45)       NOT NULL,
-   telephone         INT(9)            NOT NULL,
-   direction         VARCHAR(45)       NOT NULL,
-   password          VARCHAR(45)       NOT NULL,
-   type              ENUM()            NOT NULL,
-   
-   PRIMARY KEY (workerID)
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers (
+   customerID           INT(9)         NOT NULL,
+   username             VARCHAR(20)    NOT NULL,
+   password             VARCHAR(20)    NOT NULL,
+   name                 VARCHAR(20)    NOT NULL,
+   surname              VARCHAR(25)    NOT NULL,
+   deliveringAddress    VARCHAR(45)    NOT NULL,
+   billingAddress       VARCHAR(45)    NOT NULL,
+   mailAddress          VARCHAR(45)    NOT NULL,
+   telephone            INT(9)         NOT NULL,
+
+   PRIMARY KEY (customerID)
 );
 
 
-DROP TABLE IF EXISTS ProductTypes;
-CREATE TABLE ProductTypes (
-  productTypeID      INT(11)           NOT NULL AUTO_INCREMENT,
-  clothes            VARCHAR(45)       NOT NULL,
-  colour             VARCHAR(45)       NOT NULL,
-  style              VARCHAR           NOT NULL,
-  image              VARCHAR           NOT NULL,
-  size               VARCHAR           NOT NULL,
+DROP TABLE IF EXISTS ShoppingCarts;
+CREATE TABLE ShoppingCart (
+   customerID           INT(9)         NOT NULL,
+   productID            VARCHAR(45)    NOT NULL,
+   numItems             INT(3)         NOT NULL,
+   
+   FOREIGN KEY customerID references Customers(customerID),
+   FOREIGN KEY productID references Products(productID),
+   PRIMARY KEY (customerID, productID)
+);
 
-  PRIMARY KEY (productTypeID)
+
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders (
+   orderID              INT(9)         NOT NULL
+   customerID           INT(9)         NOT NULL,
+   date                 DATE           NOT NULL,
+   totalPrice           DECIMAL(9,2)   NOT NULL,
+
+   FOREIGN KEY customerID references Customers(customerID),
+   PRIMARY KEY (orderID)
+);
+
+
+DROP TABLE IF EXISTS SaleRecords;
+CREATE TABLE SaleRecords (
+   orderID              INT(9)         NOT NULL,
+   saleRecordID         INT(3)         NOT NULL,
+   productID            VARCHAR(45)    NOT NULL,
+   numItems             INT(3)         NOT NULL,
+   pricePerItem         DECIMAL(9,2)   NOT NULL,
+   
+   FOREIGN KEY orderID references Orders(orderID),
+   FOREIGN KEY productID references Products(productID),
+   PRIMARY KEY (orderID, orderRecordID)
 );
